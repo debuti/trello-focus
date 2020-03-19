56abfebc8cc55e50b816f814fb8c52dcfd6a2719
Borja García <user@server>
2020-03-19 13:00:24 +0000

    Extracted inlined style

--- web/__init__.py
+++ web/__init__.py
@@ -9,11 +9,7 @@ appkey = os.environ.get('APPKEY')
 appsecret = os.environ.get('APPSECRET')
 appoauth = "https://"+appname+".herokuapp.com/oauth"
 
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-                               
+         
 @app.route('/')
 @app.route('/index')
 def index():
